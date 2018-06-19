cc3e1335b235d6a99256e16d7b366867b3ee6e5c
Rebecca Govert <becci.govert@gmail.com>
2018-06-19 19:08:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:0][:yukihiro_matsumoto]=
+programmer_hash[:0][:yukihiro_matsumoto]=>{:known_for => "Ruby", :languages => ["LISP", "C"]
     
 end
 
